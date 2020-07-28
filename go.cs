e49e595a853d56e331aa21ce05cef81dd2197435
Shreekrishnam <68891747+Shreekrishnam@users.noreply.github.com>
2020-07-28 15:16:21 +0530

    Add files via upload

---
+++ go.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class go : MonoBehaviour
+{
+    public float t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t=1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(t*Input.GetAxis("Horizontal")*Time.deltaTime,t*Input.GetAxis("Jump")*Time.deltaTime,t*Input.GetAxis("Vertical")*Time.deltaTime);
+    }
+}
